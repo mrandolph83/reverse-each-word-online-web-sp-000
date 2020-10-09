4b44e3b1fa0172f63b14de5e1d5aeaf348c5420c
Matt Smith <mrandolph83@gmail.com>
2020-10-09 14:46:28 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -2,7 +2,7 @@ def reverse_each_word(sentence)
 # 1) Convert sentence to an array
 # 2) Return each word in the sentence backwards using .each
 # 3) Return each word in the sentence backwards using .collect
-  sentence.split 
+  sentence.split
   sentence.each do |words|
 
     updated_sentence << words.reverse!
